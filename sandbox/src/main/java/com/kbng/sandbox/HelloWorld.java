5d4039c5a49c487a24fa58a2b453f7b2b9a7d159
jorneivigo <jorneivigo@gmail.com>
2015-10-05 21:29:15 -0400

    initial

---
+++ sandbox/src/main/java/com/kbng/sandbox/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.kbng.sandbox;
+
+/**
+ * @author jorneivigo
+ *
+ */
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
